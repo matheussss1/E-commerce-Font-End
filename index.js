919080bce41ab94449642b3b387e2d7b439de110
Matheus Carvalho <matheusfelipe_contato@hotmail.com>
2021-02-18 19:28:27 -0300

    front-end done

--- index.js
+++ index.js
@@ -24,5 +24,52 @@
             } 
         });
 
+        //isotope filter
+
+        var $grid = $(".grid").isotope({
+            itemSelector:'.grid-item',
+            layoutMode:'fitRows'
+        });
+
+        //filter-item-on-button-click
+
+        $(".button-group").on("click", "button",function(){
+            var filterValue = $(this).attr("data-filter");
+            $grid.isotope({filter:filterValue});
+        })
+        
+        //New phone owl carousel
+        $("#new_phones .owl-carousel").owlCarousel({
+            loop:true,
+            nav:true,
+            dots:false,
+            responsive: {
+                0: {
+                    items: 2
+                },
+                600: {
+                    items: 3
+                },
+                1000: {
+                    items: 5
+                }
+            } 
+        });
+        
+        //Blog owl carousel
+        $("#blog .owl-carousel").owlCarousel({
+            loop:true,
+            nav:true,
+            dots:true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 3
+                }
+            } 
+        });
+
 
     });
